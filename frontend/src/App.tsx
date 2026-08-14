626cb0129381e6a832e87c8c657b589f17dcdde5
MrJohnAlex <judi699095@gmail.com>
2026-08-14 16:58:11 +0630

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Hello my ecommerce app</h1>
+        </section>
+    </>
+  )
+}
+
+export default App
